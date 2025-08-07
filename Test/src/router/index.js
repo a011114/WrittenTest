1bc19030143f79974aa58865cf0db8c7ae9f2152
胡攸泓 <1153424538@qq.com>
2025-08-07 17:21:26 +0800

    创建登录页面和主页，并配置路由

--- Test/src/router/index.js
+++ Test/src/router/index.js
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/login-view.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+     {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('../views/home-view.vue'),
+      meta: { keepAlive: true },
+    },
+  ],
 })
 
 export default router
