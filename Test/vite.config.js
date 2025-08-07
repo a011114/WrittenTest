7a30fd77b7e17bd9d49a1a38d002d5e9cf2c6028
胡攸泓 <1153424538@qq.com>
2025-08-07 21:49:02 +0800

    打包

--- Test/vite.config.js
+++ Test/vite.config.js
@@ -3,14 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-     Components({
+    Components({
       resolvers: [
         AntDesignVueResolver({
           importStyle: false, // css in js
@@ -18,9 +18,13 @@ export default defineConfig({
       ],
     }),
   ],
+  base: './',
+  server: {
+    open: '/login',
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
